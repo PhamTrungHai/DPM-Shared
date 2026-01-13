c2345592d7b23bd6fd992d7a3dd8b29215959d54
Pham Trung Hai <haipt@vuthao.com>
2026-01-13 15:49:19 +0700

    feat: add Prettier configuration and Husky pre-commit hook for improved code formatting

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    printWidth: 100,
+    singleQuote: true,
+    bracketSpacing: true,
+    singleAttributePerLine: true,
+};
+
+module.exports = config;
